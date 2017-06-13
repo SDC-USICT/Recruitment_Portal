e808f76fc37e7574ba566c13e2868f253843647b
Hemang Kumar <hemangsk@gmail.com>
2017-06-14 03:29:27 +0530

    views: Add index page with header and footer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var title = "Recruitment Portal";
+  var desc = "Online Portal to apply for various posts at GGSIP University";
+
+  var site = {
+    title : title,
+    desc : desc
+  }
+
+  res.render('index', { site : site});
+
 });
 
 module.exports = router;
